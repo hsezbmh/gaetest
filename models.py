1fab7cdae3a79fdeb12e24f0d92b74e18a3fef67
hsezbmh <hsezbmh@gmail.com>
2013-04-03 13:45:58 +0800

    测试写入datastore，测试handler书写

--- models.py
+++ models.py
@@ -1,7 +1,33 @@
-import datetime
 from google.appengine.ext import db
+import webapp2
+import os
+from google.appengine.ext.webapp import template
 
 class User(db.Model):
     username = db.StringProperty()
     passwd = db.StringProperty()
-    createdate = db.DateProperty()
+
+class Register(webapp2.RequestHandler):
+    def get(self):
+        template_values={}
+        path = os.path.join(os.path.dirname(__file__),'register.html')
+        self.response.out.write(template.render(path,template_values))
+    
+class AddUser(webapp2.RequestHandler):
+    def post(self):
+        user = User()
+        user.username = self.request.get('username')
+        user.passwd = self.request.get('passwd')
+        user.put()
+        self.redirect('http://localhost:8080')
+
+app = webapp2.WSGIApplication([
+                               ('/reg',Register),
+                               ('/reg/add',AddUser)
+                               ])
+
+
+    
+    
+
+
